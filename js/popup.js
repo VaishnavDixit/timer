71af42d86ef426a0ff14a76e2c364a181846f074
Vaishnav <vaishnavdixit98@gmail.com>
2022-05-13 20:03:39 +0530

    new

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,17 @@
 var startButton = document.getElementById("button");
+var timetext=document.getElementById("timedisplay");
 startButton.addEventListener("click", () => {
+	timetext.style.display="";
 	var hh = document.getElementById("iphr");
 	var mm = document.getElementById("ipmin");
 	var totalSecs = mm.value;
 	console.log(mm.value);
-	var timeout = setTimeout(timesUp(totalSecs), totalSecs * 1000);
+	var check = setInterval(function(){
+		if(totalSecs==0){
+			clearInterval(check);
+			alert("times up!");
+		}
+		timetext.innerHTML="time :"+totalSecs;
+		totalSecs-=1;
+	}, 1000);
 });
-
-function timesUp(totalSecs) {
-	alert("times up! " + totalSecs);
-}
