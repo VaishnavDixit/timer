fbeda4fec1a4452b56920a14241f96586b94f3f7
Vaishnav <vaishnavdixit98@gmail.com>
2022-05-14 01:27:37 +0530

    added functionaality to timer

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,28 @@
 var startButton = document.getElementById("button");
-var timetext=document.getElementById("timedisplay");
+var timetext = document.getElementById("timedisplay");
+var timeD = document.getElementById("timediv");
+var stopBtn=document.getElementById("stopbtn");
 startButton.addEventListener("click", () => {
-	timetext.style.display="";
-	var hh = document.getElementById("iphr");
-	var mm = document.getElementById("ipmin");
-	var totalSecs = mm.value;
-	console.log(mm.value);
-	var check = setInterval(function(){
-		if(totalSecs==0){
+	startButton.style.display = "none";
+	var hh = parseInt(document.getElementById("iphr").value);
+	var mm = parseInt(document.getElementById("ipmin").value);	
+	var totalSecs = (hh * 3600) + mm*60;
+	timeD.style.display="block";
+	timetext.innerHTML = "Time: " + Math.floor(totalSecs / 3600) + ':' + (Math.floor((totalSecs % 3600) / 60)<10?"0":"") + Math.floor((totalSecs % 3600) / 60)+":"+ (Math.floor(totalSecs % 60)<10?"0":"")+Math.floor(totalSecs % 60);
+	stopBtn.addEventListener("click",function(){
+		clearInterval(check);
+		timeD.style.display="none";
+		startButton.style.display = "";
+		//alert("times up!");
+	});
+	var check = setInterval(function () {
+		totalSecs -= 1;
+		if (totalSecs < 0) {
 			clearInterval(check);
+			timeD.style.display="none";
+			startButton.style.display = "";
 			alert("times up!");
 		}
-		timetext.innerHTML="time :"+totalSecs;
-		totalSecs-=1;
+		timetext.innerHTML = "Time: " + Math.floor(totalSecs / 3600) + ':' + (Math.floor((totalSecs % 3600) / 60)<10?"0":"") + Math.floor((totalSecs % 3600) / 60)+":"+ (Math.floor(totalSecs % 60)<10?"0":"")+Math.floor(totalSecs % 60);
 	}, 1000);
 });
