c54c68ff8d12f0c6fa3e947af5fce1d99e53b585
Vaishnav <vaishnavdixit98@gmail.com>
2022-05-13 19:01:38 +0530

    new commit

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,12 @@
-var startButton = document.getElementsByClassName("button");
-startButton[0].addEventListener("click", () => {
-	//alert("clicked");
+var startButton = document.getElementById("button");
+startButton.addEventListener("click", () => {
 	var hh = document.getElementById("iphr");
 	var mm = document.getElementById("ipmin");
 	var totalSecs = mm.value;
 	console.log(mm.value);
 	var timeout = setTimeout(timesUp(totalSecs), totalSecs * 1000);
 });
-//mm == sec
-function timesUp(mins) {
-	alert("times up!"+ mins);
+
+function timesUp(totalSecs) {
+	alert("times up! " + totalSecs);
 }
